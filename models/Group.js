fc91d6c78e932a360d3df0f8725fa77374e924d2
RyuSeongHo <ryuseongho7@naver.com>
2024-10-14 10:47:56 +0900

    Feat: "Group 모델 스키마 생성"
    Group 모델의 스키마 생성 및 Counter 모델로 id필드의 autoincrement 구현

---
+++ models/Group.js
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose';
+import Counter from './Counter.js';
+
+const GroupSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+      type: String,
+    },
+    isPublic: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    likeCount: {
+      type: Number,
+      default: 0,
+    },
+    badges: {
+      type: [String], // 배열이므로 타입은 String으로 설정
+      default: [],
+    },
+    postCount: {
+      type: Number,
+      default: 0,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    introduction: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // createdAt과 updatedAt 필드가 자동으로 관리됩니다.
+  }
+);
+
+GroupSchema.pre('save', async function () {
+  const doc = this;
+  
+  if (!doc.isNew) return; // 새 문서가 아니면 패스
+
+  try {
+    // Counter 모델을 사용해 자동 증가 id 생성
+    const counter = await Counter.findByIdAndUpdate(
+      { _id: 'groupId' },
+      { $inc: { seq: 1 } },
+      { new: true, upsert: true }
+    );
+    doc.id = counter.seq;  // 자동 증가된 id 할당
+  } catch (error) {
+    throw new Error('Error generating sequence ID: ' + error.message);
+  }
+});
+
+const Group = mongoose.model('Group', GroupSchema);
+
+
+export default Group;
