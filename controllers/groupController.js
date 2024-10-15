63617068ea544860a223f44afbf11decdec6f7ef
RyuSeongHo <ryuseongho7@naver.com>
2024-10-15 20:58:39 +0900

    Feat: "그룹 목록 가져오기 구현"
    GET api/groups 구현

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -7,6 +7,49 @@ export const createGroup = async (req, res) => {
         res.status(201).json(savedGroup);
     }
     catch (error) {
+        if(error.name == 'ValidationError') {
+            res.status(404).json({ message: '잘못된 요청입니다' });
+            return;
+        } else {
+            res.status(409).json({ message: '서버 오류가 발생했습니다' });
+        }
+    }
+};
+
+export const getGroup = async (req, res) => {
+    try {
+        let { page = 1,
+                pageSize = 10,
+                sortBy = 'latest',
+                keyword = '',
+                isPublic = true
+            } = req.query;
+
+        // 유효성 검증
+        page = isNaN(page) || page <= 0 ? 1 : Number(page);
+        pageSize = isNaN(pageSize) || pageSize <= 0 ? 10 : Number(pageSize);
+
+        //sortBy: latest | mostPosted | mostLiked |  mostBadge (정렬 기준)
+        const actions = {
+            latest: {createdAt: 'desc'},
+            mostPosted: {postCount: 'desc'},
+            mostLiked: {likeCount: 'desc'},
+            mostBadge: {badges: 'desc'},
+        };
+
+        const sortOption = actions[sortBy];
+
+        const regex = new RegExp(keyword, 'i');
+        const skip = (page - 1) * pageSize;
+
+        const foundGroup = await Group
+            .find({name: regex, isPublic: isPublic})
+            .sort(sortOption)
+            .skip(skip)
+            .limit(pageSize);
+        res.status(200).json(foundGroup);
+    }
+    catch(error) {
         res.status(409).json({ message: error.message });
     }
 };
--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -2,11 +2,13 @@ import express from 'express';
 
 import {
     createGroup,
+    getGroup
 } from '../controllers/groupController.js';
 
 const router = express.Router();
 
 // Group CRUD routes
 router.post('/', createGroup);
+router.get('/', getGroup);
 
 export default router;
